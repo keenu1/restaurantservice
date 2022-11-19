6136c458cc4736e30cdc64a01b4c917c04b7380b
keenu1 <nugrohorizki191@gmail.com>
2022-11-19 19:52:37 +0700

    update admin table

--- assets/js/main.js
+++ assets/js/main.js
@@ -169,3 +169,28 @@ function myFunction() {
         }
     }
 }
+
+/*=============== FUNCTION SEARCH TABLE===============*/
+
+function searchTable() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("Input");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("Table");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        // Hide the row initially.
+        tr[i].style.display = "none";
+
+        td = tr[i].getElementsByTagName("td");
+        for (var j = 0; j < td.length; j++) {
+            cell = tr[i].getElementsByTagName("td")[j];
+            if (cell) {
+                if (cell.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    tr[i].style.display = "";
+                    break;
+                }
+            }
+        }
+    }
+}
